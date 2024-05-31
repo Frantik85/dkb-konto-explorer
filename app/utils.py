6e803599c983fcc32492e57c69cae6873da77cbb
Daniel Findeisen <daniel.findeisen@gmail.com>
2024-05-31 08:46:46 +0200

    Adds the following functions: - filter by category in Visa view (using bootstrap-select) - filter by category in Konto view (using bootstrap-select) - editing keywords and its category assoziation - a new view to show expenses by choosing a date range - option to apply categories to the whole database - deleting keywords thus removing it from the labeled data as well

--- app/utils.py
+++ app/utils.py
@@ -1,4 +1,4 @@
-from .models import dkb_visa, KATEGORIEN
+from .models import dkb_visa, KATEGORIEN, kategorie_schlagwoerter, dkb_konto
 from . import db
 from sqlalchemy import func
 
@@ -21,6 +21,32 @@ def get_paginated_data(table_class, offset):
     return table_class.query.order_by(
         table_class.id.asc()
     ).limit(10).offset(offset).all()
+
+
+def get_paginated_konto_data(offset, limit: int = None):
+    query = db.session.query(
+        dkb_konto.Kontonummer,
+        dkb_konto.Kontoinhaber,
+        dkb_konto.Buchungsdatum,
+        dkb_konto.Wertstellung,
+        dkb_konto.Zahlungspflichtiger,
+        dkb_konto.Zahlungsempfänger,
+        dkb_konto.Verwendungszweck,
+        dkb_konto.Umsatztyp,
+        dkb_konto.IBAN,
+        dkb_konto.Betrag,       
+        KATEGORIEN.Kategorie_name
+    ).join(
+        KATEGORIEN, dkb_konto.Kategorie_id == KATEGORIEN.id
+    ).order_by(
+        func.date(dkb_konto.Buchungsdatum).desc()
+    )
+    
+    if limit is not None:
+        query = query.limit(limit)
+
+    data = query.all()
+    return data
     
 
 def get_paginated_visa_data(offset, limit: int = None):
@@ -72,10 +98,24 @@ def get_unique_non_categorized_data(limit: int = None):
     if limit is not None:
         query = query.limit(limit)
 
-    unique_descriptions = query.all()
+    query.all()
 
-    return unique_descriptions
+    return query.all()
 
 
 def get_all_categories():
     return KATEGORIEN.query.order_by(KATEGORIEN.Kategorie_name).all()
+
+
+def get_keyword_to_category_mapping():
+    query = db.session.query(
+        kategorie_schlagwoerter.Schlagwort,
+        kategorie_schlagwoerter.id,
+        kategorie_schlagwoerter.Kategorie_id,
+        KATEGORIEN.Kategorie_name
+        ).join(
+            KATEGORIEN, kategorie_schlagwoerter.Kategorie_id == KATEGORIEN.id
+        ).order_by(
+            KATEGORIEN.Kategorie_name
+        )
+    return query.all()
--- app/views/data_views.py
+++ app/views/data_views.py
@@ -1,12 +1,14 @@
-from flask import Blueprint, render_template, redirect, url_for, request, jsonify
+from flask import Blueprint, render_template, redirect, url_for, request, jsonify, flash
 from ..models import dkb_visa, dkb_konto, KATEGORIEN, kategorie_schlagwoerter
 from .. import db
 from sqlalchemy import func
 from ..utils import get_paginated_data, \
                     get_paginated_visa_data, \
+                    get_paginated_konto_data, \
                     get_unique_non_categorized_data, \
                     get_all_categories, \
-                    get_expenses_summarized_by_category
+                    get_expenses_summarized_by_category, \
+                    get_keyword_to_category_mapping
 
 data_bp = Blueprint('data', __name__)
 charts_bp = Blueprint('charts', __name__)
@@ -15,20 +17,90 @@ charts_bp = Blueprint('charts', __name__)
 @data_bp.route('/switch_tab/<string:tab_name>')
 def switch_tab(tab_name):
     active_tab = tab_name
+    categories = get_all_categories()
     if tab_name == 'dkb_visa':
         dkb_visa_data = get_paginated_visa_data(offset=0)
-        return render_template('dkb_data.html', dkb_visa_data=dkb_visa_data, active_tab=active_tab)
+        return render_template('dkb_data.html', dkb_visa_data=dkb_visa_data, active_tab=active_tab, categories=categories)
     elif tab_name == 'dkb_konto':
-        dkb_konto_data = get_paginated_data(dkb_konto, 0)
-        return render_template('dkb_data.html', dkb_konto_data=dkb_konto_data, active_tab=active_tab)
+        dkb_konto_data = get_paginated_konto_data(offset=0)
+        return render_template('dkb_data.html', dkb_konto_data=dkb_konto_data, active_tab=active_tab, categories=categories)
     elif tab_name == 'dkb_visa_categorize':
         unique_data = get_unique_non_categorized_data(20)
-        categories = get_all_categories()
         return render_template('dkb_visa_categorize.html', unique_data=unique_data, categories=categories, active_tab=active_tab)
     elif tab_name == 'charts':
         return render_template('charts.html')
+    elif tab_name == 'edit_keywords_and_categories':
+        viewing_data = get_keyword_to_category_mapping()
+        return render_template('edit_keywords_and_categories.html', viewing_data=viewing_data, categories=categories, active_tab=active_tab)
     else:
         return redirect(url_for('main.index'))
+
+
+@data_bp.route('/filter_data', methods=['POST'])
+def filter_data():
+    selected_categories = request.form.getlist('category')
+    active_tab = request.form.get('active_tab')
+    categories = get_all_categories()
+    if active_tab == 'dkb_visa':
+        # Query data based on selected categories
+        dkb_visa_data = db.session.query(
+        dkb_visa.Belegdatum,
+        dkb_visa.Beschreibung,
+        dkb_visa.id,
+        dkb_visa.Wertstellung,
+        dkb_visa.Visa_Kartennummer,
+        dkb_visa.Visa_Karteninhaber,
+        dkb_visa.Umsatztyp,
+        dkb_visa.Betrag,
+        dkb_visa.Fremdwährungsbetrag,
+        KATEGORIEN.Kategorie_name
+        ).join(
+            KATEGORIEN, dkb_visa.Kategorie_id == KATEGORIEN.id
+        ).filter(
+            dkb_visa.Kategorie_id.in_(selected_categories)
+        ).order_by(
+            func.date(dkb_visa.Belegdatum).desc()
+        ).all()
+    
+        return render_template(
+            'dkb_data.html', 
+            dkb_visa_data=dkb_visa_data, 
+            active_tab='dkb_visa', 
+            categories=categories, 
+            selected_categories=selected_categories)
+        
+    if active_tab == 'dkb_konto':
+        # Query data based on selected categories
+        dkb_konto_data = db.session.query(dkb_konto.Kontonummer,
+        dkb_konto.Kontoinhaber,
+        dkb_konto.Buchungsdatum,
+        dkb_konto.Wertstellung,
+        dkb_konto.Zahlungspflichtiger,
+        dkb_konto.Zahlungsempfänger,
+        dkb_konto.Verwendungszweck,
+        dkb_konto.Umsatztyp,
+        dkb_konto.IBAN,
+        dkb_konto.Betrag,       
+        KATEGORIEN.Kategorie_name
+        ).join(
+            KATEGORIEN, dkb_konto.Kategorie_id == KATEGORIEN.id
+        ).filter(
+            dkb_konto.Kategorie_id.in_(selected_categories)
+        ).order_by(
+            func.date(dkb_konto.Buchungsdatum).desc()
+        ).all()
+    
+        
+        return render_template(
+            'dkb_data.html', 
+            dkb_konto_data=dkb_konto_data, 
+            active_tab='dkb_konto', 
+            categories=categories, 
+            selected_categories=selected_categories)
+    
+    # Add similar logic for other tabs if needed
+    
+    return redirect(url_for('main.index'))  # Redirect to the main page if no matching tab is found
 
 
 @data_bp.route('/upload', methods=['GET', 'POST'])
@@ -53,6 +125,50 @@ def update_categories(category_id: int, keyword: str):
     except Exception as e:
         db.session.rollback()
         print(f"Error when changing categories for dkb_visa entries: {e}")
+
+
+@data_bp.route('/update_keyword_category', methods=['POST'])
+def update_keyword_category():
+    entry_id = request.form['entry_id']
+    category_id = request.form['category']
+    keyword = request.form['category_keyword']
+
+    keyword = kategorie_schlagwoerter.query.filter_by(id=entry_id).first()
+    if keyword is not None:
+        try:
+            keyword.Kategorie_id = category_id
+            db.session.commit()
+        except Exception as e:
+            db.session.rollback()
+            print(f"Error adding keyword entry: {e}")
+
+    update_categories(category_id, keyword)
+
+    return redirect(request.referrer)
+
+
+@data_bp.route('/delete_keyword_entry', methods=['POST'])
+def delete_keyword_entry():
+    entry_id = request.form['entry_id']
+    keyword = kategorie_schlagwoerter.query.filter_by(id=entry_id).first()
+    
+    if keyword:
+        try:
+            # Update dkb_visa entries that have this keyword's category
+            db.session.query(dkb_visa).filter_by(Kategorie_id=keyword.Kategorie_id).update({"Kategorie_id": 1})
+            db.session.commit()
+            
+            # Now delete the keyword entry
+            db.session.delete(keyword)
+            db.session.commit()
+            
+            flash('Entry deleted successfully.', 'success')
+        except Exception as e:
+            db.session.rollback()
+            flash(f'Error deleting entry: {e}', 'danger')
+    
+    return redirect(request.referrer)
+
 
 
 @data_bp.route('/update_category', methods=['POST'])
@@ -110,8 +226,8 @@ def add_new_category():
         return jsonify({"error": "Category name is required."}), 400
 
 
-@data_bp.route('/apply_category_to_descriptions', methods=['GET'])
-def apply_category_to_descriptions():
+@data_bp.route('/apply_category_to_visa', methods=['GET'])
+def apply_category_to_visa():
     schlagwoerter = kategorie_schlagwoerter.query.all()
 
     for schlagwort in schlagwoerter:
@@ -124,6 +240,20 @@ def apply_category_to_descriptions():
 
     return "Categories applied to descriptions successfully."
 
+
+@data_bp.route('/apply_category_to_konto', methods=['GET'])
+def apply_category_to_konto():
+    schlagwoerter = kategorie_schlagwoerter.query.all()
+
+    for schlagwort in schlagwoerter:
+        matching_entries = dkb_konto.query.filter(func.lower(dkb_konto.Zahlungsempfänger).like('%' + schlagwort.Schlagwort.lower() + '%')).all()
+
+        for entry in matching_entries:
+            entry.Kategorie_id = schlagwort.Kategorie_id
+
+    db.session.commit()
+
+    return "Categories applied to descriptions successfully."
 
 @charts_bp.route('/charts', methods=['GET', 'POST'])
 def view_chart():
@@ -180,3 +310,9 @@ def get_max_date_range():
         return jsonify({'min_date': min_date, 'max_date': max_date})
     else:
         return jsonify({'error': 'No data available'}), 404
+   
+    
+@charts_bp.route('/edit_keywords_and_categories', methods=['GET'])
+def edit_keywords_and_categories():
+    categories = get_all_categories()
+    return render_template('edit_keywords_and_categories.html', categories=categories)
